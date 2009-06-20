86ce2d7d52478a47dd6fca90517eb322316fd321
Mike Boers <github@mikeboers.com>
2009-06-19 21:19:25 -0400

    WIP

---
+++ nitrogen/formalchemy_patch.py
@@ -0,0 +1,36 @@
+
+import sys
+sys.path.append('lib')
+
+from sqlalchemy import *
+from elixir import *
+
+import formalchemy as fa
+# import formalchemy.fields
+
+metadata.bind = 'sqlite://'
+metadata.bind.echo = False
+
+class Test(Entity):
+    id = Field(Integer, primary_key=True)
+    string = Field(String)
+    text = Field(Text)
+    bool = Field(Boolean)
+
+setup_all(True)
+
+
+
+cls = fa.FieldRenderer
+print cls
+
+@property
+def new_name(self):
+    return 'fa-' + self.field.name
+cls.name = new_name
+
+print cls.name
+
+fs = fa.FieldSet(Test)
+
+print fs.render()
