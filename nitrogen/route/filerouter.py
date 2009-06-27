557c709fc118fa4388291e71c2e8a4dd2d6ed7e6
Mike Boers <github@mikeboers.com>
2009-06-26 20:14:47 -0400

    Moved route module. Worked on various routers.

--- nitrogen/route/filerouter.py
+++ nitrogen/route/filerouter.py
@@ -1,4 +1,18 @@
-"""Module for wsgi dispatchers."""
+"""Module file based router.
+
+A File router is initialized with a path. When it is called (as a WSGI app)
+it takes the first unrouted segment of the path and looks for a python file
+that corresponds to the base path and the segment (ie.
+path + segment + '.py'). It executes that file, and looks for an
+object by the name of the app_key attribute (defaults to "app") of the router.
+
+The router keeps an eye on the modification time of the python file, to reload
+it if it changes.
+
+Note that the path is not joined to the segment (with os.path.join), but it is
+simply contatenated.
+
+"""
 
 import os
 import re
@@ -8,15 +22,17 @@ from . import get_routed, get_route_segment, NotFoundError
 
 log = logging.getLogger(__name__)
 
-class Directory(object):
+class FileRouter(object):
     
-    def __init__(self, path, app_key='app'):
+    def __init__(self, path, app_key='app', reload_modifications=True):
+        """Initialize the file router."""
         self.path = path
         self.app_key = app_key
         self.apps = {}
+        self.reload_modifications = reload_modifications
     
     def build_path(self, name):
-        return '%s%s.py' % (self.path, name)
+        return "%s%s.py" % (self.path, name)
     
     def load_app(self, environ, path):
         if not os.path.exists(path):
@@ -34,7 +50,7 @@ class Directory(object):
         if path not in self.apps:
             log.debug("Loading app %r at %r." % (name, path))
             self.load_app(environ, path)
-        if os.path.getmtime(path) != self.apps[path][0]:
+        if self.reload_modifications and os.path.getmtime(path) != self.apps[path][0]:
             log.debug("App %r modified. Reloading." % name)
             self.load_app(environ, path)
         
---
+++ nitrogen/route/reflector.py
@@ -0,0 +1,27 @@
+"""Reflection based WSGI router.
+
+This class first pulls off an unrouted segment that it will handle. Then, it
+looks for an attribute on itself named "do_<segment>", and calls it.
+
+If it can't find anything, it throws a NotFound error.
+
+It is written so that you can nest reflectors.
+
+"""
+
+from . import get_routed, get_route_segment, NotFoundError
+
+class Reflector(object):
+    
+    def __init__(self, environ, start):
+        self.environ = environ
+        self.start = start
+    
+    def __iter__(self):
+        segment = get_route_segment(self.environ) or 'index'
+        method_name = 'do_' + segment
+        if not hasattr(self, method_name):
+            raise NotFoundError(get_routed(environ))
+        method = getattr(self, method_name)
+        for x in method(self.environ, self.start):
+            yield x
