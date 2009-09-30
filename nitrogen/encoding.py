daed4a3ac661ca48f17918e37e991fa561b58cca
Mike Boers <github@mikeboers.com>
2009-09-30 09:49:32 -0400

    WIP

---
+++ nitrogen/encoding.py
@@ -0,0 +1,27 @@
+
+def utf8_encoder(app):
+    """WSGI middlewear that encodes everything to a UTF-8 string.
+    
+    Forces text/* content types to have a UTF-8 charset.
+    If there is no Content-Type, it adds a utf8 plain text one.
+    """
+    def inner(environ, start):
+        def inner_start(status, headers):
+            has_type = False
+            for i, h in enumerate(headers):
+                if h[0] == 'Content-Type':
+                    has_type = True
+                    if h[1].startswith('text'):
+                        if 'charset' not in h[1]:
+                            headers[i] = (h[0], h[1].strip() + '; charset=UTF-8')
+                        elif 'UTF-8' not in h[1]:
+                            raise ValueError('Content-Type header has non UTF-8 charset: %r.' % h[1])
+            if not has_type:
+                headers.append(('Content-Type', 'text/plain; charset=UTF-8'))
+            start(status, headers)
+        for x in app(environ, inner_start):
+            if not isinstance(x, unicode):
+                x = unicode(x, 'utf8', 'replace')
+            # Should this be ascii? Then all the unicode characters go as XML refs.
+            yield x.encode('utf8', 'xmlcharrefreplace')
+    return inner
