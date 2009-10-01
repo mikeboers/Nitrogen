6eef27f41e4adbb45370cf17fc4c7e009addafc0
Mike Boers <github@mikeboers.com>
2009-09-30 21:53:09 -0400

    - Wrote test for utf8 encoder middlewear.

--- nitrogen/encoding.py
+++ nitrogen/encoding.py
@@ -1,3 +1,11 @@
+# content-encoding: utf-8
+
+"""Module for UTF-8 encoding WSGI middlewear.
+
+It is currently not very tolerant about other encodings being used behind it's
+back, but if I ever need them I will deal with them at that time.
+
+"""
 
 def utf8_encoder(app):
     """WSGI middlewear that encodes everything to a UTF-8 string.
@@ -9,12 +17,12 @@ def utf8_encoder(app):
         def inner_start(status, headers):
             has_type = False
             for i, h in enumerate(headers):
-                if h[0] == 'Content-Type':
+                if h[0].lower() == 'content-type':
                     has_type = True
-                    if h[1].startswith('text'):
-                        if 'charset' not in h[1]:
+                    if h[1].startswith('text/'):
+                        if 'charset' not in h[1].lower():
                             headers[i] = (h[0], h[1].strip() + '; charset=UTF-8')
-                        elif 'UTF-8' not in h[1]:
+                        elif 'utf-8' not in h[1].lower() and 'utf8' not in h[1].lower():
                             raise ValueError('Content-Type header has non UTF-8 charset: %r.' % h[1])
             if not has_type:
                 headers.append(('Content-Type', 'text/plain; charset=UTF-8'))
@@ -25,3 +33,23 @@ def utf8_encoder(app):
             # Should this be ascii? Then all the unicode characters go as XML refs.
             yield x.encode('utf8', 'xmlcharrefreplace')
     return inner
+
+
+def test_utf8_encoder():
+    """Nose test, checking that plaintext is returned."""
+
+    from webtest import TestApp
+    
+    @utf8_encoder
+    def app(environ, start):
+        start('200 OK', [('Content-Type', 'text/plain')])
+        yield u'¡™£¢∞§¶•ªº'
+    app = TestApp(app)
+
+    res = app.get('/')
+    assert res.headers['content-type'] == 'text/plain; charset=UTF-8', 'Wrong content type.'
+    assert res.body == u'¡™£¢∞§¶•ªº'.encode('utf8'), 'Not encoded properly.'
+
+if __name__ == '__main__':
+    from test import run
+    run()
