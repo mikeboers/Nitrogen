daac4e9e6544c181748977eb1e9365b447f7366d
Mike Boers <github@mikeboers.com>
2009-10-31 22:34:13 -0400

    wip

--- nitrogen/middleware.py
+++ nitrogen/middleware.py
@@ -1,16 +1,25 @@
 
 
-# Setup path for local evaluation.
-# When copying to another file, just change the __package__ to be accurate.
+# Setup path for local evaluation. Do not modify anything except for the name
+# of the toplevel module to import at the very bottom.
 if __name__ == '__main__':
-    import sys
-    __package__ = 'nitrogen'
-    sys.path.insert(0, __file__[:__file__.rfind('/' + __package__.split('.')[0])])
-    __import__(__package__)
+    def __local_eval_setup(root, debug=False):
+        global __package__
+        import os, sys
+        file = os.path.abspath(__file__)
+        sys.path.insert(0, file[:file.find(root)].rstrip(os.path.sep))
+        name = file[file.find(root):]
+        name = '.'.join(name[:-3].split(os.path.sep)[:-1])
+        __package__ = name
+        if debug:
+            print ('Setting up local environ:\n'
+                   '\troot: %(root)r\n'
+                   '\tname: %(name)r' % locals())
+        __import__(name)
+    __local_eval_setup('nitrogen', True)
+
 
 import logging
-
-from .views import render, TYPE_HEADER_HTML
 
 from .request import as_request
 from .compressor import compressor
@@ -49,7 +58,7 @@ def output_buffer(app):
     
     return inner
 
-def not_found_catcher(app):
+def not_found_catcher(app, view_environ):
     """Displays the _404.tpl template along with a "404 Not Found" status if a
     HttpNotFound is thrown within the app that it wraps. This error is
     normally thrown by routers.
@@ -61,9 +70,9 @@ def not_found_catcher(app):
         except HttpNotFound as e:
             log.exception('caught 404 Not Found')
             start('404 Not Found', [TYPE_HEADER_HTML])
-            yield render('_404.tpl')
+            yield view_environ.render('_404.tpl')
     return inner        
 
 if __name__ == '__main__':
-    from nitrogen.test import run
-    run()
+    import nitrogen.test
+    nitrogen.test.run()
