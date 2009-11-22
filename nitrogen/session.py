1f9ce3368e553e7f78178869b8dd9e6026e0fb5f
Mike Boers <github@mikeboers.com>
2009-11-21 22:37:03 -0500

    changes to session middleware

--- nitrogen/session.py
+++ nitrogen/session.py
@@ -1,14 +1,14 @@
 
 import beaker.middleware
 
-def session_wrapper(app, auto=True, secret='secret', data_dir='/tmp'):
+def session_wrapper(app, auto_save=True, secret='secret', data_dir='/tmp'):
     session_opts = {
         'session.key': 'session',
         'session.secret': secret,
         'session.data_dir': data_dir,
         'session.lock_dir': data_dir,
         'session.type': 'file',
-        'session.auto': auto,
+        'session.auto': auto_save,
     }
     return beaker.middleware.SessionMiddleware(app, session_opts)
 
