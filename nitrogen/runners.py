472970070e5c8796069fb41f959dd3d1a22e5f36
Mike Boers <github@mikeboers.com>
2009-12-10 00:34:44 -0500

    - moved wsgi handlers into wsgi module - depreciated runners

--- nitrogen/runners.py
+++ nitrogen/runners.py
@@ -1,36 +1,12 @@
 """WSGI application runners."""
 
 
-import atexit
-import itertools
 import logging
-import multiprocessing
-import os
-import threading
-from wsgiref.handlers import CGIHandler as _CGIHandler
-from wsgiref.simple_server import make_server as _make_server
 
-from flup.server.fcgi import WSGIServer as _FCGIHandler
-from flup.server.fcgi_fork import WSGIServer as _FCGIForkHandler
-
-from . import error
+from .wsgi import CGIHandler, FCGIThreadHandler, FCGIForkHandler, SocketHandler
 
 
 log = logging.getLogger(__name__)
-
-
-class CGIHandler(_CGIHandler):
-    
-    error_status = error.DEFAULT_ERROR_HTTP_STATUS
-    error_headers = error.DEFAULT_ERROR_HTTP_HEADERS
-    error_body = error.DEFAULT_ERROR_BODY
-    
-    def __init__(self, app):
-        _CGIHandler.__init__(self)
-        self.app = app
-    
-    def run(self):
-        self.run(self.app)
 
 
 def run_via_cgi(app):
@@ -39,96 +15,19 @@ def run_via_cgi(app):
     Parameters:
         app -- The WSGI app to run.
     """
-    
+    log.warning('run_via_cgi is depreciated')
     CGIHandler(app).run()
 
 
+def run_via_fcgi_thread(app, **kwargs):    
+    log.warning('run_via_fcgi_thread is depreciated')
+    FCGIThreadHandler(app, **kwargs).run()
 
 
-class FCGIForkHandler(_FCGIForkHandler):
-    
-    def __init__(self, *args, **kwargs):
-        self._setup = kwargs.pop('setup', None)
-        self._teardown = kwargs.pop('teardown', None)
-        self._pid = os.getpid()
-        super(FCGIForkHandler, self).__init__(*args, **kwargs)
-        
-    def _child(self, *args):
-        
-        # Update the "current process". The multiprocessing module does this
-        # by setting the _current_process of the process module to the current
-        # process. We have to fake this.
-        proc = multiprocessing.current_process()
-        # proc._identity = ()
-        # proc._daemonic = False
-        proc._name = 'FcgiFork-%d' % os.getpid()
-        proc._parent_pid = self._pid
-        proc._popen = None
-        proc._counter = itertools.count(1)
-        proc._children = set()
-        # proc._authkey = AuthenticationString(os.urandom(32))
-        proc._tempdir = None
-        
-        # Run the utilities that setup the multiprocessing environment so that
-        # managers still work properly.
-        multiprocessing.util._finalizer_registry.clear()
-        multiprocessing.util._run_after_forkers()
-        
-        if self._setup:
-            self._setup()
-        
-        ret = super(FCGIForkHandler, self)._child(*args)
-        
-        if self._teardown:
-            self._teardown()
-        
-        return ret
-        
-def run_via_fcgi_thread(app, min_spare=1, max_spare=5, max_total=10, **kwargs):
-    """Run a web application via a FastCGI interface of a web server.
-
-    Parameters:
-        app -- The WSGI app to run.
-        multithreaded -- Run this application in a multithreaded environment
-            if nessesary. Will be running several copies of the app at once 
-            if the server is under load.
-    """
-    _FCGIHandler(app, multithreaded=bool(thread), minSpare=min_spare,
-        maxSpare=max_spare, maxThreads=max_total, **kwargs).run()
-
-
-def run_via_fcgi_fork(app, min_spare=1, max_spare=5, max_total=25,
-    max_requests=100, setup=None, teardown=None, **kwargs):
-    """Run a web application via a FastCGI interface of a web server.
-
-    Parameters:
-        app -- The WSGI app to run.
-        multithreaded -- Run this application in a multithreaded environment
-            if nessesary. Will be running several copies of the app at once 
-            if the server is under load.
-    """
-    FCGIForkHandler(app, minSpare=min_spare, maxSpare=max_spare,
-        maxChildren=max_total, maxRequests=max_requests, setup=setup,
-        teardown=teardown, **kwargs).run()
-
+def run_via_fcgi_fork(app, **kwargs):
+    log.warning('run_via_fcgi_fork is depreciated')
+    FCGIForkHandler(app, **kwargs).run()
 run_via_fcgi = run_via_fcgi_fork
-
-
-class SocketHandler(object):
-    
-    def __init__(self, app, host='', port=8000):
-        self.app = app
-        self.host = host
-        self.port = port
-    
-    def make_server(self):
-        return _make_server(self.host, self.port, self.app)
-    
-    def handle_request(self):
-        self.make_server().handle_request()
-    
-    def run(self):
-        self.make_server().serve_forever()
 
 
 def run_via_socket(app, host='', port=8000, once=False):
@@ -141,6 +40,7 @@ def run_via_socket(app, host='', port=8000, once=False):
         once -- Only accept a single connection.
     """
     
+    log.warning('run_via_socket is depreciated')
     handler = SocketHandler(app, host, port)
     if once:
         handler.handle_request()
---
+++ nitrogen/wsgi.py
@@ -0,0 +1,103 @@
+"""WSGI application runners."""
+
+
+import itertools
+import multiprocessing
+import os
+from wsgiref.handlers import CGIHandler as _CGIHandler
+from wsgiref.simple_server import make_server as _make_server
+
+from flup.server.fcgi import WSGIServer as _FCGIThreadHandler
+from flup.server.fcgi_fork import WSGIServer as _FCGIForkHandler
+
+from . import error
+
+
+class CGIHandler(_CGIHandler):
+
+    error_status = error.DEFAULT_ERROR_HTTP_STATUS
+    error_headers = error.DEFAULT_ERROR_HTTP_HEADERS
+    error_body = error.DEFAULT_ERROR_BODY
+
+    def __init__(self, app):
+        _CGIHandler.__init__(self)
+        self.app = app
+
+    def run(self):
+        self.run(self.app)
+
+
+class FCGIThreadHandler(_FCGIThreadHandler):
+
+    def __init__(self, app, min_spare=1, max_spare=5, max_threads=50):
+        super(FCGIThreadHandler, self).__init__(app, minSpare=min_spare,
+            maxSpare=max_spare, maxThreads=max_threads)
+
+
+class FCGIForkHandler(_FCGIForkHandler):
+
+    def __init__(self, app, min_spare=1, max_spare=5, max_children=50,
+        max_requests=0, setup=None, teardown=None):
+
+        self._setup_child = setup
+        self._teardown_child = teardown
+        self._pid = os.getpid()
+
+        super(FCGIForkHandler, self).__init__(app, minSpare=min_spare,
+            maxSpare=max_spare, maxChildren=max_children,
+            maxRequests=max_requests)
+
+    def setup_child(self):
+        if self._setup_child:
+            self._setup_child()
+
+    def teardown_child(self):
+        if self._teardown_child:
+            self._teardown_child()
+
+    def _child(self, *args):
+
+        # Update the "current process". The multiprocessing module does this
+        # by setting the _current_process of the process module to the current
+        # process. We have to fake this.
+        proc = multiprocessing.current_process()
+        # proc._identity = ()
+        # proc._daemonic = False
+        proc._name = 'FcgiFork-%d' % os.getpid()
+        proc._parent_pid = self._pid
+        proc._popen = None
+        proc._counter = itertools.count(1)
+        proc._children = set()
+        # proc._authkey = AuthenticationString(os.urandom(32))
+        proc._tempdir = None
+
+        # Run the utilities that setup the multiprocessing environment so that
+        # managers still work properly.
+        multiprocessing.util._finalizer_registry.clear()
+        multiprocessing.util._run_after_forkers()
+
+        self.setup_child()
+
+        ret = super(FCGIForkHandler, self)._child(*args)
+
+        self.teardown_child()
+
+        return ret
+
+
+class SocketHandler(object):
+
+    def __init__(self, app, host='', port=8000):
+        self.app = app
+        self.host = host
+        self.port = port
+
+    def make_server(self):
+        return _make_server(self.host, self.port, self.app)
+
+    def handle_request(self):
+        self.make_server().handle_request()
+
+    def run(self):
+        self.make_server().serve_forever()
+
