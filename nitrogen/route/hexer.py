7ef7408079b49b249f550bd6e190424b987db814
Mike Boers <github@mikeboers.com>
2009-12-14 14:09:56 -0500

    - wrote two path modifiers

---
+++ nitrogen/route/hexer.py
@@ -0,0 +1,45 @@
+
+from .tools import Router, TestApp
+from .maprouter import MapRouter
+
+class Hexer(Router):
+    
+    def __init__(self, app):
+        self.app = app
+    
+    def route_step(self, path):
+        try:
+            path = path[1:].decode('hex')
+        except:
+            pass
+        return self.app, path, {}
+    
+    def generate_step(self, data):
+        return '', self.app
+    
+    def modify_path(self, path):
+        print 'Hexer.modify_path', path
+        return '/' + path.encode('hex')
+
+
+def test_hexer():
+    
+    a = TestApp('one')
+    b = TestApp('two')
+    c = TestApp('three')
+    map = MapRouter('x')
+    map.update(dict(a=a, b=b, c=c))
+    
+    # print map.route('/a')
+    hexer = Hexer(map)
+    
+    print repr(map.url_for(x='a'))
+    
+    url = hexer.url_for(x='a')
+    print repr(url)
+    
+    print hexer.route(url)
+
+if __name__ == '__main__':
+    from ..test import run
+    run()
---
+++ nitrogen/route/signer.py
@@ -0,0 +1,59 @@
+
+from pprint import pprint
+import logging
+
+from ..uri.query import Query
+from .maprouter import MapRouter
+from .tools import Router, TestApp
+
+
+log = logging.getLogger(__name__)
+
+
+class Signer(Router):
+    
+    def __init__(self, app, key=(__file__ + __name__)):
+        self.app = app
+        self.key = key
+    
+    def route_step(self, path):
+        try:
+            path, rawquery = path.rsplit('/', 1)
+            query = Query(rawquery)
+            query['path'] = path
+            if query.verify(key=self.key):
+                return self.app, path, {}
+            else:
+                log.warning('path %r did not match signature %r' % (path, rawquery))
+        except:
+            log.exception('error while verifying path signature')
+    
+    def generate_step(self, data):
+        return '', self.app
+    
+    def modify_path(self, path):
+        print 'Signer.modify_path', path
+        query = Query()
+        query['path'] = path
+        query.sign(key=self.key, add_time=False, nonce_bits=64)
+        del query['path']
+        return path + '/' + str(query)
+
+
+def test_hexer():
+    
+    a = TestApp('one')
+    b = TestApp('two')
+    c = TestApp('three')
+    map = MapRouter('x')
+    map.update(dict(a=a, b=b, c=c))
+    hexer = Signer(map)
+    
+    url = hexer.url_for(x='a')
+    print repr(url)
+    pprint(hexer.route(url))
+    # pprint(hexer.route('/a/whatever'))
+
+if __name__ == '__main__':
+    from ..test import run
+    run()
