8f622321a75f576c4da7de87373a806ec47faad1
Mike Boers <github@mikeboers.com>
2010-01-06 22:59:37 -0500

    Markdown replaces linebreaks with <br />.

--- nitrogen/view/defaults.py
+++ nitrogen/view/defaults.py
@@ -13,12 +13,12 @@ place.
 import json
 import pprint
 
-from markdown import markdown
 import webhelpers.text
 import webhelpers.html
+from markdown import markdown
 
-from tools import urlify_name, clean_html
-
+from .tools import urlify_name, clean_html
+from ..markdown import github_markdown
 
 context = {}
 
@@ -26,7 +26,7 @@ context['repr'] = repr
 context['sorted'] = sorted
 
 context['json'] = json.dumps
-context['markdown'] = lambda x: clean_html(markdown(x))
+context['markdown'] = lambda x: clean_html(markdown(github_markdown(x)))
 context['format_date'] = lambda d, f: (d.strftime(f) if d else '')
 context['randomize'] = lambda x: sorted(x, key=lambda y: random.random())
 context['nl2br'] = lambda s: s.replace("\n", "<br />")
@@ -46,6 +46,10 @@ context['clean_html'] = clean_html
 def test_markdown():
     print 'Markdown of "**bold**":'
     print markdown('**bold**')
+    
+    print markdown(github_markdown('''hi\nthere
+![Link text here](http://farm1.static.flickr.com/159/345009210_1f826cd5a1_m.jpg)
+'''))
 
 
 if __name__ == '__main__':
