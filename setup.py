18f5bfd8383181f3c169b92bf3d1c7ac0c8876d4
Mike Boers <github@mikeboers.com>
2010-02-12 14:47:07 -0500

    Wrote basic `setup.py`.

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = "nitrogen",
+    version = "0.1a.dev",
+    packages = find_packages(),
+    
+    install_requires=[],
+    
+    include_package_data=True,
+        
+    author="Mike Boers",
+    author_email="nitrogen@mikeboers.com",
+    license="New BSD"
+)
