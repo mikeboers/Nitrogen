895105188f3685e4e7670cdc018b48c603529133
Mike Boers <github@mikeboers.com>
2010-03-09 14:56:50 -0500

    Cleaned up model context a little.
    - Session class is in it's own file.
    - declarative Base has some more methods.
    - renamed the wsgi wrapper method, but I may
      change it again soon.

--- nitrogen/model/__init__.py
+++ nitrogen/model/__init__.py
@@ -1,4 +1,5 @@
 """nitrogen.models package"""
 
+# This is magical, and not cool.
 import logging
 logger = logging.getLogger('sqlalchemy').setLevel(logging.WARNING)
--- nitrogen/model/context.py
+++ nitrogen/model/context.py
@@ -5,27 +5,18 @@ import threading
 
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.orm.session import Session as _BaseSession
 
-from .declarative import build_declarative_base
-
-
-class _Session(_BaseSession):
-    
-    def lock(self, exclusive=False):
-        self.execute("BEGIN %S" % 'EXCLUSIVE' if exclusive else "IMMEDIATE")
-    
-    def write_lock(self):
-        self.lock(False)
-    
-    def read_lock(self):
-        self.lock(True)
+from .declarative import declarative_base
+from .session import Session
 
 
 class ModelContext(object):
     """A helper to contain the basic parts of a database connections.
     
-    Attrs:
+    By default this also uses the nitrogen extended Session (with locking) and
+    declarative Base (with extra session functions).
+    
+    Attributes:
         engine
         metadata
         Base -- declarative base class
@@ -34,11 +25,13 @@ class ModelContext(object):
     
     """
     
-    def __init__(self, engine=None, autocommit=False, echo=False):
+    def __init__(self, bind=None, autocommit=False, echo=False):
         """Initialize the environment.
         
         Args:
-            engine -- The engine or string to bind to.
+            bind -- The engine or string to bind to.
+            autocommit -- sessionmaker kwarg autocommit.
+            echo -- create_engine kwarg echo.
         
         """
         
@@ -47,14 +40,14 @@ class ModelContext(object):
         self._local_sessions = []
         
         self.engine = None
-        self.Session = sessionmaker(class_=_BaseSession, autocommit=autocommit, autoflush=True)
+        self.Session = sessionmaker(class_=Session, autocommit=autocommit, autoflush=True)
         self.session = self.local_session()
         self.metadata = MetaData()
-        self.Base = build_declarative_base(metadata=self.metadata)
+        self.Base = declarative_base(metadata=self.metadata)
         self.echo = echo
         
-        if engine:
-            self.bind(engine)
+        if bind:
+            self.bind(bind)
     
     def bind(self, engine):
         """Bind to an engine or string."""
@@ -65,7 +58,7 @@ class ModelContext(object):
         self.Session.configure(bind=engine)
     
     def create_tables(self):
-        """Create all the tables associated with this environ."""
+        """Create all the tables associated with this context."""
         self.metadata.create_all(self.engine)
     
     def local_session(self):
@@ -76,7 +69,7 @@ class ModelContext(object):
         thread will be different from each other.
         
         This should not be called on a request-basis. This is mainly for other
-        environ contructs to have their own thread-local local session.
+        context-like contructs to have their own thread-local session.
         
         """
         
@@ -84,11 +77,28 @@ class ModelContext(object):
         self._local_sessions.append(s)
         return s
     
-    def wsgi_reset(self, app):
-        """Reset all the thread-local sessions generated."""
-        def ModelContext_wsgi_reset_app(environ, start):
+    def wsgi_fixtures(self, app):
+        """WSGI middleware to setup/teardown the model context per request.
+        
+        Currently resets the thread-local sessions generated
+        
+        """
+        def ModelContext_wsgi_fixtures_app(environ, start):
             for x in app(environ, start):
                 yield x
             for s in self._local_sessions:
                 s.close()
-        return ModelContext_wsgi_reset_app
+                # Lets be even more aggresive... It seems that MAYBE a session
+                # would persist when using flup thread pools.
+                s.remove()
+        return ModelContext_wsgi_fixtures_app
+    
+    def wsgi_reset(self, app):
+        # Warn that we have changed the name. This can be removed once
+        # all my web apps have moved on.
+        msg = '%s.wsgi_reset is depreciated.' % self.__class__.__name__
+        try:
+            raise DepreciationWarning(msg)
+        except DepreciationWarning:
+            self.log.warning(msg, exc_info=sys.exc_info()))
+        return self.wsgi_fixtures(app)
