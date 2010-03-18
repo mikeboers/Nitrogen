20b1763e49375f2534a73f6a872d38a13d226148
Mike Boers <github@mikeboers.com>
2010-03-18 11:52:09 -0400

    webio.query.parse_query does not delay

--- nitrogen/webio/query.py
+++ nitrogen/webio/query.py
@@ -6,16 +6,7 @@ from ..uri.query import Query
 ENVIRON_KEY = 'nitrogen.get'
 
 def parse_query(environ, key=ENVIRON_KEY):
-    """Get the query string parsed into a MultiMap.
-    
-    This multimap is lazily parsed when it is actually accessed. It is also
-    cached in the environment, so changes to one will be reflected to later
-    results of this function.
-    
-    """
+    """Return the query for this request."""
     if key not in environ:
-        def parse_query_generator():
-            query = environ.get('QUERY_STRING', '')
-            return Query(query).allitems()
-        environ[key] = multimap.DelayedMultiMap(parse_query_generator)
+        environ[key] = Query(environ.get('QUERY_STRING', ''))
     return environ[key]
