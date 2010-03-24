8469682ea00cade1c8d4c4096c6e1b2b9075963a
Mike Boers <github@mikeboers.com>
2010-03-24 13:05:15 -0400

    Query parsing uses charset/errors.

--- nitrogen/webio/query.py
+++ nitrogen/webio/query.py
@@ -5,8 +5,8 @@ from ..uri.query import Query
 
 ENVIRON_KEY = 'nitrogen.get'
 
-def parse_query(environ, key=ENVIRON_KEY):
+def parse_query(environ, charset=None, errors=None, key=ENVIRON_KEY):
     """Return the query for this request."""
     if key not in environ:
-        environ[key] = Query(environ.get('QUERY_STRING', ''))
+        environ[key] = Query(environ.get('QUERY_STRING', ''), charset=charset, decode_errors=errors)
     return environ[key]
