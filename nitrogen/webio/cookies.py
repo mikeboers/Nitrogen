b906eb2e9767505d798e293d27a6c920c38a9f92
Mike Boers <github@mikeboers.com>
2010-03-25 18:34:06 -0400

    Replaces quote function with something sensible.

--- nitrogen/webio/cookies.py
+++ nitrogen/webio/cookies.py
@@ -230,85 +230,31 @@ DECODE_ERRORS = 'replace'
 # quoted with a preceeding '\' slash.
 #
 # These are taken from RFC2068 and RFC2109.
-#       _legal_chars       is the list of chars which don't require "'s
+#       _safe_chars       is the list of chars which don't require "'s
 #       _encoding_map       hash-table for fast quoting
 #
-_legal_chars = string.ascii_letters + string.digits + "!#$%&'*+-.^_`|~"
 
-_encoding_map = {
-    '\000' : '\\000',  '\001' : '\\001',  '\002' : '\\002',
-    '\003' : '\\003',  '\004' : '\\004',  '\005' : '\\005',
-    '\006' : '\\006',  '\007' : '\\007',  '\010' : '\\010',
-    '\011' : '\\011',  '\012' : '\\012',  '\013' : '\\013',
-    '\014' : '\\014',  '\015' : '\\015',  '\016' : '\\016',
-    '\017' : '\\017',  '\020' : '\\020',  '\021' : '\\021',
-    '\022' : '\\022',  '\023' : '\\023',  '\024' : '\\024',
-    '\025' : '\\025',  '\026' : '\\026',  '\027' : '\\027',
-    '\030' : '\\030',  '\031' : '\\031',  '\032' : '\\032',
-    '\033' : '\\033',  '\034' : '\\034',  '\035' : '\\035',
-    '\036' : '\\036',  '\037' : '\\037',
-    
-    '"' : '\\"',       '\\' : '\\\\',
-    
-    '\177' : '\\177',  '\200' : '\\200',  '\201' : '\\201',
-    '\202' : '\\202',  '\203' : '\\203',  '\204' : '\\204',
-    '\205' : '\\205',  '\206' : '\\206',  '\207' : '\\207',
-    '\210' : '\\210',  '\211' : '\\211',  '\212' : '\\212',
-    '\213' : '\\213',  '\214' : '\\214',  '\215' : '\\215',
-    '\216' : '\\216',  '\217' : '\\217',  '\220' : '\\220',
-    '\221' : '\\221',  '\222' : '\\222',  '\223' : '\\223',
-    '\224' : '\\224',  '\225' : '\\225',  '\226' : '\\226',
-    '\227' : '\\227',  '\230' : '\\230',  '\231' : '\\231',
-    '\232' : '\\232',  '\233' : '\\233',  '\234' : '\\234',
-    '\235' : '\\235',  '\236' : '\\236',  '\237' : '\\237',
-    '\240' : '\\240',  '\241' : '\\241',  '\242' : '\\242',
-    '\243' : '\\243',  '\244' : '\\244',  '\245' : '\\245',
-    '\246' : '\\246',  '\247' : '\\247',  '\250' : '\\250',
-    '\251' : '\\251',  '\252' : '\\252',  '\253' : '\\253',
-    '\254' : '\\254',  '\255' : '\\255',  '\256' : '\\256',
-    '\257' : '\\257',  '\260' : '\\260',  '\261' : '\\261',
-    '\262' : '\\262',  '\263' : '\\263',  '\264' : '\\264',
-    '\265' : '\\265',  '\266' : '\\266',  '\267' : '\\267',
-    '\270' : '\\270',  '\271' : '\\271',  '\272' : '\\272',
-    '\273' : '\\273',  '\274' : '\\274',  '\275' : '\\275',
-    '\276' : '\\276',  '\277' : '\\277',  '\300' : '\\300',
-    '\301' : '\\301',  '\302' : '\\302',  '\303' : '\\303',
-    '\304' : '\\304',  '\305' : '\\305',  '\306' : '\\306',
-    '\307' : '\\307',  '\310' : '\\310',  '\311' : '\\311',
-    '\312' : '\\312',  '\313' : '\\313',  '\314' : '\\314',
-    '\315' : '\\315',  '\316' : '\\316',  '\317' : '\\317',
-    '\320' : '\\320',  '\321' : '\\321',  '\322' : '\\322',
-    '\323' : '\\323',  '\324' : '\\324',  '\325' : '\\325',
-    '\326' : '\\326',  '\327' : '\\327',  '\330' : '\\330',
-    '\331' : '\\331',  '\332' : '\\332',  '\333' : '\\333',
-    '\334' : '\\334',  '\335' : '\\335',  '\336' : '\\336',
-    '\337' : '\\337',  '\340' : '\\340',  '\341' : '\\341',
-    '\342' : '\\342',  '\343' : '\\343',  '\344' : '\\344',
-    '\345' : '\\345',  '\346' : '\\346',  '\347' : '\\347',
-    '\350' : '\\350',  '\351' : '\\351',  '\352' : '\\352',
-    '\353' : '\\353',  '\354' : '\\354',  '\355' : '\\355',
-    '\356' : '\\356',  '\357' : '\\357',  '\360' : '\\360',
-    '\361' : '\\361',  '\362' : '\\362',  '\363' : '\\363',
-    '\364' : '\\364',  '\365' : '\\365',  '\366' : '\\366',
-    '\367' : '\\367',  '\370' : '\\370',  '\371' : '\\371',
-    '\372' : '\\372',  '\373' : '\\373',  '\374' : '\\374',
-    '\375' : '\\375',  '\376' : '\\376',  '\377' : '\\377'
-    }
+# The set of characters that do not require quoting.
+_safe_chars_set = set(string.ascii_letters + string.digits + "!#$%&'*+-.^_`|~")
+# The set of characters that do not need ecaping iff we quote the string.
+_no_escape_chars_set = set(' (),/;:<>=?@[]{}')
+# How to translate ALL characters.
+_encoding_map = dict()
+for i in range(256):
+    c = chr(i)
+    if c in _safe_chars_set or c in _no_escape_chars_set:
+        continue
+    _encoding_map[c] = '\\%03o' % i
+_encoding_map['"'] = '\\"'
+_encoding_map['\\'] = '\\\\'
 
-_idmap = ''.join(chr(x) for x in xrange(256))
-        
+
 def quote(to_quote):
-    #
-    # If the string does not need to be double-quoted,
-    # then just return the string.  Otherwise, surround
-    # the string in doublequotes and precede quote (with a \)
-    # special characters.
-    #
-    if not to_quote.translate(_idmap, _legal_chars):
+    if not set(to_quote).difference(_safe_chars_set):
         return to_quote
     else:
         return '"' + ''.join(map(_encoding_map.get, to_quote, to_quote)) + '"'
-# end quote
+
 
 _octal_re = re.compile(r"\\[0-3][0-7][0-7]")
 _quote_re = re.compile(r"[\\].")
@@ -356,7 +302,7 @@ def unquote(to_unquote):
     return ''.join(res)
 
 
-_ATTRIBUTE_PAIRS = {
+_ATTRIBUTES = {
     "path": "Path",
     "comment": "Comment",
     "domain": "Domain",
@@ -365,9 +311,6 @@ _ATTRIBUTE_PAIRS = {
     "version": "Version",
     "http_only": "HttpOnly",
 }
-
-_ATTRIBUTES = dict(_ATTRIBUTE_PAIRS)
-
 
 
 class Cookie(object):
@@ -448,17 +391,17 @@ class Cookie(object):
 # result, the parsing rules here are less strict.
 #
 
-_legal_charsPatt  = r"[\w\d!#%&'~_`><@,:/\$\*\+\-\.\^\|\)\(\?\}\{\=]"
+_safe_charsPatt  = r"[\w\d!#%&'~_`><@,:/\$\*\+\-\.\^\|\)\(\?\}\{\=]"
 _cookie_re = re.compile(
     r"(?x)"                       # This is a Verbose pattern
     r"(?P<key>"                   # Start of group 'key'
-    ""+ _legal_charsPatt +"+?"    # Any word of at least one letter, nongreedy
+    ""+ _safe_charsPatt +"+?"    # Any word of at least one letter, nongreedy
     r")"                          # End of group 'key'
     r"\s*=\s*"                    # Equal Sign
     r"(?P<val>"                   # Start of group 'val'
     r'"(?:[^\\"]|\\.)*"'            # Any doublequoted string
     r"|"                            # or
-    ""+ _legal_charsPatt +"*"       # Any word or empty string
+    ""+ _safe_charsPatt +"*"       # Any word or empty string
     r")"                          # End of group 'val'
     r"\s*;?"                      # Probably ending in a semi-colon
     )
@@ -518,7 +461,7 @@ class Container(multimap.MutableMultiMap):
                 key = key.encode('ascii')
             except UnicodeError:
                 raise ValueError("illegal key value: %r." % key)
-        if "" != key.translate(_idmap, _legal_chars):
+        if set(key).difference(_safe_chars_set):
             raise ValueError("illegal key value: %r" % key)
         return key
     
