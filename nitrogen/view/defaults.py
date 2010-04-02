0f25954e89356df7096b2f284a3c5447bfef570c
Mike Boers <github@mikeboers.com>
2010-04-02 11:31:23 -0400

    Cleaning up view context defaults.

--- nitrogen/view/defaults.py
+++ nitrogen/view/defaults.py
@@ -17,10 +17,11 @@ import webhelpers.text
 import webhelpers.html
 from markdown import markdown
 
-from .util import urlify_name, clean_html, smart_html_escape
-from ..markdown import github_markdown
 from .. import lipsum
+from ..markdown import markdown
 from ..uri.query import encode as query_encode
+from .util import urlify_name, clean_html, smart_html_escape
+
 
 context = {}
 
@@ -30,7 +31,7 @@ context['html_escape'] = smart_html_escape
 context['h'] = smart_html_escape
 
 context['json'] = json.dumps
-context['markdown'] = lambda x: clean_html(markdown(github_markdown(x)))
+context['markdown'] = markdown
 context['format_date'] = lambda d, f: (d.strftime(f) if d else '')
 context['randomize'] = lambda x: sorted(x, key=lambda y: random.random())
 context['nl2br'] = lambda s: s.replace("\n", "<br />")
@@ -50,14 +51,6 @@ context['lipsum'] = lipsum
 
 context['query_encode'] = query_encode
 
-
-def test_markdown():
-    print 'Markdown of "**bold**":'
-    print markdown('**bold**')
-    
-    print markdown(github_markdown('''hi\nthere
-![Link text here](http://farm1.static.flickr.com/159/345009210_1f826cd5a1_m.jpg)
-'''))
 
 
 if __name__ == '__main__':
