4dda416372f3712bd4574225a5ae6c6041cf9c16
Mike Boers <github@mikeboers.com>
2010-06-13 14:03:15 -0400

    Removed some unused constants.

--- nitrogen/view/__init__.py
+++
@@ -1,7 +0,0 @@
-'''nitrogen.view package'''
-
-# Some constants for the two most common Content-Type headers. These are being
-# depreciated, as the request object (which is in use much more often) does
-# this all for us.
-TYPE_HEADER_HTML = ('Content-Type', 'text/html;charset=UTF-8')
-TYPE_HEADER_TEXT = ('Content-Type', 'text/plain;charset=UTF-8')
