8bbd084d8ee93c3cd4ffae909958d316cffe84ef
Mike Boers <github@mikeboers.com>
2010-08-23 13:08:50 -0400

    Spread webio package contents around.

--- nitrogen/webio/query.py
+++ nitrogen/webio/query.py
@@ -1,12 +1,1 @@
-
-import multimap
-
-from ..uri.query import Query
-
-ENVIRON_KEY = 'nitrogen.req.get'
-
-def parse_query(environ, charset=None, errors=None, key=ENVIRON_KEY):
-    """Return the query for this request."""
-    if key not in environ:
-        environ[key] = Query(environ.get('QUERY_STRING', ''), charset=charset, decode_errors=errors)
-    return environ[key]
+from ..uri.query import parse_query, Query
