0689f45774b45191e53aa4d8b28c3e9dd18af309
Mike Boers <github@mikeboers.com>
2010-08-23 13:20:37 -0400

    Disabled output_buffer.

--- nitrogen/wsgi/buffer.py
+++ nitrogen/wsgi/buffer.py
@@ -1,31 +1,13 @@
+import logging
+
+
+log = logging.getLogger(__name__)
+
 
 def buffer_output(app):
-    """WSGI middleware which buffers all output before sending it on.
-    
-    The entire sub-app is completely exhausted before anything is returned
-    from this. This allows you to call for WSGI start after you output, and
-    multiple times (only the args from the last call are sent on).
-    
-    This behaviour is exhibited by quite a few other WSGI middlewares.
-    
-    """
-    
-    class buffer_output_app(object):
-        
-        def __init__(self, environ, start):
-            self.environ = environ
-            self.start = start
-            self.start_args = None
-            
-        def inner_start(self, *args):
-            self.start_args = args
-            
-        def __iter__(self, ):
-            output = list(app(self.environ, self.inner_start))
-            self.start(*self.start_args)
-            return iter(output)
-    
-    return buffer_output_app
-
+    def _buffer_output(*args):
+        log.warning('buffer_output has been deprecated. Output was not buffered.')
+        return app(*args)
+    return _buffer_output
 
 output_buffer = buffer_output
