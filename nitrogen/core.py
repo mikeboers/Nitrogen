847363ef979a2e84a6c24839605a65565b34acf9
Mike Boers <github@mikeboers.com>
2010-08-26 13:16:23 -0400

    ImgSizer app mixin.

--- nitrogen/core.py
+++ nitrogen/core.py
@@ -1,13 +1,15 @@
 
+from .app import Core
 from .db.app import DBAppMixin
 from .exception import ExceptionAppMixin
+from .imgsizer import ImgSizerAppMixin
 from .logs import LoggingAppMixin
 from .session import SessionAppMixin
 from .view.app import ViewAppMixin
-from .app import Core
 
 # Be careful about the order of these.
 class App(
+    ImgSizerAppMixin, # Must be before View
     ViewAppMixin,
     SessionAppMixin,
     DBAppMixin,
@@ -17,12 +19,13 @@ class App(
 ):
     pass
 
+del Core
 del DBAppMixin
 del ExceptionAppMixin
+del ImgSizerAppMixin
 del LoggingAppMixin
 del SessionAppMixin
 del ViewAppMixin
-del Core
 
 from .route import ReRouter
 from .status import abort, redirect
