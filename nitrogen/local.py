123ffcabb6fa783b1524a55dd3dce52ad33a13db
Mike Boers <github@mikeboers.com>
2010-08-28 18:12:22 -0400

    Fix Local class to work with older werkzeug.

--- nitrogen/local.py
+++ nitrogen/local.py
@@ -1,11 +1,16 @@
 
 import collections
-from werkzeug.local import release_local, Local, LocalManager, LocalStack, allocate_lock
+from werkzeug.local import release_local, Local, LocalManager, LocalStack, allocate_lock, get_ident
 from .proxy import Proxy
 
 class Local(Local):
-
-    # Just adding a __dict__ property to the object.
+    
+    # We are extending this class for the only purpose of adding a __dict__
+    # attribute, so that this will work nearly identically to the builtin
+    # threading.local class.
+    
+    # Not adding any more attributes, but we don't want to actually add a dict.
+    __slots__ = ()
     
     def __init__(self):
         super(Local, self).__init__()
@@ -13,7 +18,13 @@ class Local(Local):
     
     @property
     def __dict__(self):
-        return self.__storage__[self.__ident_func__()]
+        # The __ident_func__ attribute is added after the 0.6.2 release (at
+        # this point it is still in the development branch). This lets us
+        # work with both versions.
+        try:
+            return self.__storage__[self.__ident_func__()]
+        except AttributeError:
+            return self.__storage__[get_ident()]
     
     def __call__(self, name):
         return Proxy(lambda: getattr(self, name))
