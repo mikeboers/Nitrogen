d90ac9ede2a2568ae455f1bc4f841f3b496a590a
Mike Boers <github@mikeboers.com>
2010-09-01 12:35:58 -0400

    Add docstring to apache middleware.

--- nitrogen/route/apache.py
+++ nitrogen/route/apache.py
@@ -2,6 +2,13 @@
 from ..uri import URI
 
 def setup_apache_path_info(app):
+    """Clears out the SCRIPT_NAME, and sets PATH_INFO to the value in REQUEST_URI.
+    
+    This is effective if you are redirecting all requests that do not refer to
+    existing files into a Python process, and want the URLs to be relative to
+    the server root.
+    
+    """
     def setup_apache_path_info_app(environ, start):
         if 'REQUEST_URI' in environ:
             path = str(URI(environ['REQUEST_URI']).path)
