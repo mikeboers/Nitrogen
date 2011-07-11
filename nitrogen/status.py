8d3c4f3296fcaedf982d50e89bbb868745590c9b
Mike Boers <github@mikeboers.com>
2011-07-10 19:05:36 -0700

    Removed a bunch of unused status code.

--- nitrogen/status.py
+++ nitrogen/status.py
@@ -41,6 +41,11 @@ class _Mixin(object):
                    'code': self.code,
                    'server': environ['SERVER_NAME'],
                    'body': body }
+    
+    # A few things to make them look more like Werkzeug Responses.
+    @property
+    def status_code(self):
+        return self.code
 
                    
 
@@ -71,155 +76,6 @@ def abort(code, *args, **kwargs):
 def redirect(location, code=303, *args, **kwargs):
     raise exceptions[code](location, *args, **kwargs)
 
-
-def code_to_message(code):
-    """Convert a integer code into the canonical message.
-    
-    Examples:
-        >>> code_to_message(200)
-        'OK'
-        >>> code_to_message(404)
-        'Not Found'
-    
-    """
-    try:
-        return _code_to_message[int(code)]
-    except KeyError:
-        raise ValueError(code)
-
-
-def message_to_code(msg):
-    """Convert a status message into the coresponding integer code.
-    
-    Is case insenstive.
-    
-    Examples:
-        >>> message_to_code('OK')
-        200
-        >>> message_to_code('ok')
-        200
-        >>> message_to_code('not found')
-        404
-    
-    """
-    try:
-        return _message_to_code[str(msg).lower()]
-    except KeyError:
-        raise ValueError(msg)
-
-
-def canonicalize_message(msg):
-    """Return the canonical version of the HTTP status message.
-    
-    Raises ValueError if it can't do it.
-    
-    Examples:
-        
-        >>> canonicalize_message('not found')
-        'Not Found'
-        
-    """
-    return code_to_message(message_to_code(msg))
-
-
-def resolve_status(status, canonicalize=False, strict=False):
-    """Resolve a given object into the status that it should represent.
-    
-    Tries to anticipate what you are thinking. If you pass an int, it returns
-    the proper message along with it. If you pass a message, it returns the
-    proper int along with (the properly cased) message. None is returned as
-    '200 OK'. Anthing else is returned unchanged.
-    
-    Note that we do NOT check to make sure statuses that are untouched are
-    valid or not.
-    
-    Examples:
-    
-        >>> resolve_status(None)
-        '200 OK'
-        >>> resolve_status(200)
-        '200 OK'
-        >>> resolve_status("OK")
-        '200 OK'
-        >>> resolve_status('200 OK')
-        '200 OK'
-        
-        >>> resolve_status(404)
-        '404 Not Found'
-        >>> resolve_status("not found")
-        '404 Not Found'
-        
-        >>> resolve_status(401)
-        '401 Unauthorized'
-        >>> resolve_status('UNAUTHORIZED')
-        '401 Unauthorized'
-        
-        >>> resolve_status('999 Not in List')
-        '999 Not in List'
-        >>> resolve_status('999 Not in List', strict=True)
-        Traceback (most recent call last):
-        ...
-        ValueError: can't resolve status '999 Not in List'
-        >>> resolve_status('200 OK', strict=True)
-        '200 OK'
-        
-        >>> resolve_status('200 Custom Message')
-        '200 Custom Message'
-        >>> resolve_status('200 Custom Message', canonicalize=True)
-        '200 OK'
-        
-        >>> resolve_status('200')
-        '200 OK'
-        
-    """
-    
-    # None implies 200.
-    if status is None:
-        return '200 OK'
-    
-    # Strings of numbers should be numbers
-    try:
-        status = int(status)
-    except ValueError:
-        if canonicalize:
-            m = re.match(r'^(\d+)(\s|$)', str(status))
-            if m:
-                status = int(m.group(1))
-    
-    # Convert it to a code if we can.
-    try:
-        status = message_to_code(status)
-    except ValueError:
-        pass
-    
-    # See if status is a status code.
-    try:
-        return '%d %s' % (status, code_to_message(status))
-    except ValueError:
-        pass
-    
-    # Can't find it...
-    if strict:
-        code, msg = None, None
-        m = re.match(r'^(\d+) (.+)$', status)
-        if m:
-            code, msg = m.groups()
-        if not code or not msg or _code_to_message.get(int(code)) != msg:
-            raise ValueError('can\'t resolve status %r' % status)
-    return status
-
-
-def status_resolver(app, canonicalize=False, strict=False):
-    def _status_resolver(environ, start):
-        log('Status_resolver has been deprecated.')
-        return app(environ, start)
-    return _status_resolver
-
-
-# Depricated
-def catch_any_status(app, lookup=None):
-    from .exception import catcher
-    return catcher(app, lookup=lookup)
 
 
 
