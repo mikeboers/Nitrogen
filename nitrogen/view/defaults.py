9906aeb8bc819bbeb5e4fbb49bf0f6121b57e43d
Mike Boers <github@mikeboers.com>
2011-08-17 22:59:14 -0700

    Fuzzy time deals with future/past.

--- nitrogen/view/defaults.py
+++ nitrogen/view/defaults.py
@@ -54,21 +54,26 @@ context['query_encode'] = query_encode
 def fuzzy_time(d, now=None):
     now = now or datetime.datetime.utcnow()
     diff = now - d
-    s = diff.seconds
-    if diff.days > 7 or diff.days < 0:
-        return d.strftime('%B %d, %Y')
-    elif diff.days == 1:
-        return '1 day ago'
-    elif diff.days > 1:
-        return '{0} days ago'.format(diff.days)
+    s = int(diff.total_seconds())
+    future = s < 0
+    days, s = divmod(abs(s), 60 * 60 * 24)
+    prefix = 'in ' if future else ''
+    postfix = '' if future else ' ago'
+    if days > 7:
+        return 'on ' + d.strftime('%B %d, %Y')
+    elif days == 1:
+        out = '1 day'
+    elif days > 1:
+        out = '{0} days'.format(diff.days)
     elif s <= 1:
         return 'just now'
     elif s < 60:
-        return '{0} seconds ago'.format(s)
+        out = '{0} seconds'.format(s)
     elif s < 3600:
-        return '{0} minutes ago'.format(s/60)
+        out = '{0} minutes'.format(s/60)
     else:
-        return '{0} hours ago'.format(s/3600)
+        out = '{0} hours'.format(s/3600)
+    return prefix + out + postfix
 
 context['fuzzy_time'] = fuzzy_time
 
