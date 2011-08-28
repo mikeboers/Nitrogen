cd2461b957757edd7ed6d6c6f7b6b4f983f9d2f2
Mike Boers <github@mikeboers.com>
2011-08-28 10:50:46 -0700

    Turn markdown.github into Nl2Br extension.

--- nitrogen/view/markdown.py
+++ nitrogen/view/markdown.py
@@ -28,62 +28,94 @@ import os
 import logging
 import re
 
-from markdown import markdown as _markdown
-
-try:
-    import pygments
-    has_pygments = True
-except ImportError:
-    has_pygments = False
+import markdown as _markdown
+from markdown import Markdown, load_extension
+from markdown.extensions.codehilite import CodeHilite
 
 log = logging.getLogger(__name__)
 
 
-pre_extraction_re = re.compile(r'<pre>.*?</pre>', re.MULTILINE | re.DOTALL)
-italic_re = re.compile(r'(?! {4}|\t)\w+_\w+_\w[\w_]*')
-newline_re = re.compile(r'^[\w\<][^\n]*(\n+)', re.MULTILINE)
-pre_insert_re = re.compile(r'{github-([0-9a-f]{32})\}')
+class Nl2BrExtension(_markdown.Extension):
+    
+    class Preprocessor(_markdown.preprocessors.Preprocessor):
+        
+        pre_extraction_re = re.compile(r'<pre>.*?</pre>', re.MULTILINE | re.DOTALL)
+        newline_re = re.compile(r'^[\w\<][^\n]*(\n+)', re.MULTILINE)
+        pre_insert_re = re.compile(r'{nl2br-([0-9a-f]{32})\}')
+        
+        def _replace(self, m):
+            print m.groups()
+            return '[[math]]'
+        def run(self, lines):
+            
+            text = '\n'.join(lines)
+            
+            # Extract pre blocks
+            extractions = {}
+            def pre_extraction_callback(matchobj):
+                token = os.urandom(16).encode('hex')
+                extractions[token] = matchobj.group(0)
+                return "{nl2br-%s}" % token
+            text = self.pre_extraction_re.sub(pre_extraction_callback, text)
+
+            # in very clear cases, let newlines become <br /> tags
+            def newline_callback(matchobj):
+                if len(matchobj.group(1)) == 1:
+                    return matchobj.group(0).rstrip() + '  \n'
+                else:
+                    return matchobj.group(0)
+            text = self.newline_re.sub(newline_callback, text)
+
+            # Insert pre block extractions
+            def pre_insert_callback(matchobj):
+                return extractions[matchobj.group(1)]
+            text = self.pre_insert_re.sub(pre_insert_callback, text)
+
+            return text.splitlines()
+    
+    def extendMarkdown(self, md, md_globals):
+        md.preprocessors.add('nl2br', self.Preprocessor(), '<html_block')
 
 
-def github_markdown(text):
 
-    # Extract pre blocks
-    extractions = {}
-    def pre_extraction_callback(matchobj):
-        token = os.urandom(16).encode('hex')
-        extractions[token] = matchobj.group(0)
-        return "{github-%s}" % token
-    text = re.sub(pre_extraction_re, pre_extraction_callback, text)
-
-    # prevent foo\_bar\_baz from ending up with an italic word in the middle
-    # def italic_callback(matchobj):
-    #     if len(re.sub(r'[^_]', '', matchobj.group(0))) > 1:
-    #         return matchobj.group(0).replace('_', '\_')
-    #     else:
-    #         return matchobj.group(0)
-    # text = re.sub(italic_re, italic_callback, text)
-
-    # in very clear cases, let newlines become <br /> tags
-    def newline_callback(matchobj):
-        if len(matchobj.group(1)) == 1:
-            return matchobj.group(0).rstrip() + '  \n'
-        else:
-            return matchobj.group(0)
-    text = re.sub(newline_re, newline_callback, text)
-
-    # Insert pre block extractions
-    def pre_insert_callback(matchobj):
-        return extractions[matchobj.group(1)]
-    text = re.sub(pre_insert_re, pre_insert_callback, text)
-
-    return text
+# Monkey patch CodeHilight to have no default language
+old_get_lang = CodeHilite._getLang
+def new_get_lang(self):
+    old_get_lang(self)
+    self.lang = self.lang or 'text'
+CodeHilite._getLang = new_get_lang
 
 
-def markdown(text, github=True, codehilight=has_pygments):
-    ext = ['codehilite'] if has_pygments else []
-    if github:
-        text = github_markdown(text)
-    return _markdown(text, ext)
+extensions = dict(
+    nl2br=Nl2BrExtension,
+)
+default_extensions = dict(
+    nl2br=True,
+    codehilite=True,
+    mathjax=True,
+)
+
+
+def markdown(text, **custom_exts):
+    
+    final_exts = []
+    exts = default_extensions.copy()
+    exts.update(custom_exts)
+    print exts
+    for name, include in exts.iteritems():
+        if include:
+            ext = extensions.get(name)
+            ext = ext() if ext else load_extension(name)
+            if ext:
+                final_exts.append(ext)
+            else:
+                log.warning('could not find extension %r' % name)
+        
+    md = Markdown(extensions=final_exts,
+                  safe_mode=False, 
+                  output_format='xhtml')
+    return md.convert(text)
+    
 
 
 
