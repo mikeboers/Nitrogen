17335d71357a76c5ba650794f8ec049be6a2ff32
Mike Boers <github@mikeboers.com>
2012-03-19 23:41:11 -0700

    Password hash uses PKCS5.

--- nitrogen/password.py
+++ nitrogen/password.py
@@ -1,3 +1,4 @@
+from __future__ import division
 
 import hashlib
 import hmac
@@ -6,27 +7,72 @@ import time
 import urllib
 import urlparse
 
+import tomcrypt.pkcs5
+
+
+# This object will get be updated with tuples of (num_iters, time_to_hash) every
+# time pkcs5 is used.
+_pkcs5_timings = []
+
+
+def _get_pkcs5_frequency():
+    
+    # Setup initial timings.
+    while len(_pkcs5_timings) < 3:
+        start_time = time.time()
+        x = tomcrypt.pkcs5.pkcs5('password', salt='salt', iteration_count=1024, hash='sha256')
+        _pkcs5_timings.append((1024, time.time() - start_time))
+    
+    return sum(n / t for n, t in _pkcs5_timings) / len(_pkcs5_timings)
+
+
+def _timed_pkcs5(input, salt, duration):
+    
+    # Determine how many iterations we should do.
+    iteration_count = int(duration * _get_pkcs5_frequency())
+    
+    start_time = time.time()
+    
+    output = tomcrypt.pkcs5.pkcs5(
+        input,
+        salt=salt,
+        iteration_count=iteration_count,
+        hash='sha256'
+    )
+    
+    # Update our timing estimators only if we were faster than expected.
+    actual_duration = time.time() - start_time
+    if actual_duration < duration:
+        _pkcs5_timings.append((iteration_count, actual_duration))
+        _pkcs5_timings.sort(key=lambda x: x[0] / x[1])
+        _pkcs5_timings[:] = _pkcs5_timings[-5:]
+    
+    return iteration_count, output
+    
+
 class PasswordHash(object):
     """
     
-    basic example:
+    Basic example:
     
         >>> h = PasswordHash()
         >>> h.set('password')
-    
         >>> h.check('password')
         True
         >>> h.check('wrong')
         False
     
-    unicode:
-        >>> h.set(u'password')
-        >>> h.check(u'password')
+    Version 0.1 (RebelHouse and PixRay):
+    
+        >>> h = PasswordHash('01129802e44542fbd7eabe52691e6cdb6810865e2982daa29481a6521678faa5512fe23594997caae715b455f8bf88f647ea3ca68548981854f5094d89681f914300002fc0'.decode('hex'))
+        >>> h.check('password')
         True
-        >>> h.check(u'wrong')
+        >>> h.check('wrong')
         False
+        >>> h.should_reset()
+        True
         
-    verifying version 1.0:
+    Version 1.0 (full Python pseudo timed PKCS5):
         
         >>> h = PasswordHash('v=1.0&num=4716&salt=78101bf4c50e5c359282feadf4eac583bbdf100fcfff15a7760c307c791ea4be&hash=db1d24423c1af72a7df6b48fc91f65c95de968884cb6a812368436e31dd52ada')
         >>> h.check('password')
@@ -34,19 +80,26 @@ class PasswordHash(object):
         >>> h.check('wrong')
         False
         >>> h.should_reset()
+        True
+    
+    Version 2.0 (timed PKCS5)
+    
+        >>> h = PasswordHash('v=2.0&num=79190&salt=b2fd88056ce0cfdc8e9ef97dab90416d27a50163b093ebbf787e5cb6d4be4bd0&hash=0ce5f5abbbd0f1919e27985c963789e1808b0769a36479255bb606ae16a85381')
+        >>> h.check('password')
+        True
+        >>> h.check('wrong')
         False
         
     """
     
-    CURRENT_VERSION = '1.0'
-    MIN_TIME = 0.25
-    MIN_ITER = 2**10
+    current_version = '2.0'
+    min_time = 0.25
     
-    def __init__(self, state=None, password=None, min_time=None, min_iter=None):
+    def __init__(self, state=None, password=None, min_time=None):
         
-        self.min_time = min_time or self.MIN_TIME
-        self.min_iter = min_iter or self.MIN_ITER
-        
+        if min_time is not None:
+            self.min_time = min_time
+
         self.num_iter = None
         self.salt = None
         self.hash = None
@@ -78,7 +131,7 @@ class PasswordHash(object):
     def __str__(self):
         return urllib.urlencode([('v', self.version), ('num', self.num_iter or 0),
             ('salt', (self.salt or '').encode('hex')), ('hash', (self.hash or
-            '').encode('hex'))]))
+            '').encode('hex'))])
     
     def __repr__(self):
         return '%s(%r)' % (self.__class__.__name__, str(self))
@@ -86,7 +139,7 @@ class PasswordHash(object):
     def resalt(self):
         self.salt = hashlib.sha256(os.urandom(8192)).digest()
     
-    def set(self, password, best_of=1):    
+    def set(self, password, best_of=3):    
         self.resalt()
         rounds = []
         password = password.encode('utf8')
@@ -95,28 +148,17 @@ class PasswordHash(object):
             rounds.append((self.num_iter, str(self)))
         rounds.sort()
         self.restore_state(rounds[-1][1])
-        self.version = self.CURRENT_VERSION
+        self.version = self.current_version
         
     def _set(self, password):
-        timer = time.time
-        hasher = hashlib.sha256
-        
-        num_iter = 0
-        end_time = timer() + self.min_time
-        
-        blob = hmac.new(self.salt, password, hasher).digest()
-        while num_iter < self.min_iter or timer() < end_time:
-            num_iter += 1
-            blob = hasher(blob).digest()
-        
-        self.num_iter = num_iter
-        self.hash = blob
+        self.num_iter, self.hash = _timed_pkcs5(password, self.salt, self.min_time)
     
     def check(self, password):
         start_time = time.time()
         out = {
             '0.1': self._check_v0_1,
             '1.0': self._check_v1_0,
+            '2.0': self._check_v2_0,
         }[self.version](password.encode('utf8'))
         self.check_time = time.time() - start_time
         return out
@@ -142,8 +184,16 @@ class PasswordHash(object):
             blob = hasher(blob).digest()
         return blob == self.hash
     
+    def _check_v2_0(self, password):
+        return self.hash == tomcrypt.pkcs5.pkcs5(
+            password,
+            salt=self.salt,
+            iteration_count=self.num_iter,
+            hash='sha256'
+        )
+    
     def should_reset(self):
-        if self.version < self.CURRENT_VERSION:
+        if self.version < self.current_version:
             return True
         if self.num_iter < self.min_iter:
             return True
@@ -153,16 +203,10 @@ class PasswordHash(object):
             return self.check_time / self.min_time < 0.67
 
 
-def test_old_timed_hash_compatibility():
+if __name__ == '__main__':
+    import getpass
+    password = getpass.getpass('password: ')
+    hasher = PasswordHash()
+    hasher.set(password)
+    print str(hasher)
     
-    hashes = [
-        '01129802e44542fbd7eabe52691e6cdb6810865e2982daa29481a6521678faa5512fe23594997caae715b455f8bf88f647ea3ca68548981854f5094d89681f914300002fc0',
-        '016291a4faf036a2030f2117e4f8e54e50f187687a8844c961d8b5f3a1b64bfde7c8b07ed179f05ce524a9bbae49690395ef9735275f330ef97a64d3b21560debc00002f40',
-        '0188921cdafba1df52879f841ade086e58ff5050c0b8882f4a73df037b85820f23ec259361e226a255282fd62748d62b8c8e9793d31457f19dbdc4d36b4715f57300002f80']
-    for hash in hashes:
-        hash = hash.decode('hex')
-        h = PasswordHash(hash)
-        assert h.check('password')
-        assert not h.check('wrong')
-        assert h.should_reset()
-
