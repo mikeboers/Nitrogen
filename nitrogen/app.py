2646bbbb7470670c697d850f8f6f57e4eab89bc0
Mike Boers <github@mikeboers.com>
2012-03-21 17:40:03 -0700

    Remove old text blobs; apps should implement this.

--- nitrogen/app.py
+++ nitrogen/app.py
@@ -187,11 +187,9 @@ def _build_final_class():
     from .exception import ExceptionAppMixin
     from .wtforms.app import FormAppMixin
     from .imgsizer import ImgSizerAppMixin
-    # from .js import JavaScriptAppMixin
     from .logs import LoggingAppMixin
     from .session import SessionAppMixin
     from .sqlalchemy.app import SQLAlchemyAppMixin
-    # from .textblobs import TextBlobAppMixin
     from .tracker import TrackerAppMixin
     from .view.app import ViewAppMixin
     from .cookies import CookieAppMixin
@@ -199,7 +197,6 @@ def _build_final_class():
     # Be careful about the order of these.
     class App(
         ImgSizerAppMixin, # Needs View
-        # TextBlobAppMixin, # Needs Form and CRUD.
         FormAppMixin, # Needs View
         CRUDAppMixin, # Needs View and SQLAlchemy
         TrackerAppMixin, # Needs View (and maybe session)
